e6438ee1a15dfe81656166f70bb8e5c25fe15181
suprkar <115251486+suprkar@users.noreply.github.com>
2024-04-21 17:29:19 +0530

    Create NewCollections.jsx

---
+++ frontend/public/src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import './NewCollections.css'
+import { useState } from 'react'
+
+import Item from '../Item/Item'
+const NewCollections = () => {
+
+  const [new_collections,setNew_Collections] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/newcollections').then((res)=>res.json()).then((data)=>setNew_Collections(data));
+  },[])
+
+  return (
+    <div className='newcollections'>
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {
+            new_collections.map((item,i)=> {
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default NewCollections
