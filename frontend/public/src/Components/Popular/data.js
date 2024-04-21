7b9864b45acf8d1d0a9c3a2bb51988de0af3f027
suprkar <115251486+suprkar@users.noreply.github.com>
2024-04-21 17:31:56 +0530

    Create data.js

---
+++ frontend/public/src/Components/Popular/data.js
@@ -0,0 +1,48 @@
+import p1_img from '../Assets/1_small.png';
+import p2_img from '../Assets/2_small.png';
+import p3_img from '../Assets/3_small.png';
+import p4_img from '../Assets/4_small.png';
+
+
+let data_product = 
+[
+    {
+        id: 1,
+        name: "Umbro Running shoes",
+        category: "shoe",
+        image: p1_img,
+        new_price: 100.0,
+        old_price: 120.0,
+    },
+
+    {
+        id: 2,
+        name: "Kookabura English Willow Bat",
+        category: "cricket",
+        image: p2_img,
+        new_price: 150.0,
+        old_price: 175.0,
+    },
+
+    {
+        id: 3,
+        name: "SG Cricket Pads",
+        category: "cricket",
+        image: p3_img,
+        new_price: 70.0,
+        old_price: 90.0,
+    },
+
+    {
+        id: 4,
+        name: "Bazuca Football",
+        category: "football",
+        image: p4_img,
+        new_price: 50.0,
+        old_price: 60.0,
+    },
+
+    
+]
+
+export default data_product;
