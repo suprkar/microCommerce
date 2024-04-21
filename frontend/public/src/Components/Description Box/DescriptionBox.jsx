75b80680330411f7e0fc676dc9bffa03590ca9f7
suprkar <115251486+suprkar@users.noreply.github.com>
2024-04-21 17:23:03 +0530

    Create DescriptionBox.jsx

---
+++ frontend/public/src/Components/Description Box/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='description-box'>
+      <div className="description-box-navigator">
+        <div className="description-box-nav-box">Description</div>
+        <div className="description-box-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="description-box-description">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo urna molestie at elementum eu facilisis sed odio morbi. Platea dictumst quisque sagittis purus sit amet volutpat consequat mauris. Nunc sed augue lacus viverra vitae congue eu consequat ac. Felis donec et odio pellentesque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo urna molestie at elementum eu facilisis sed odio morbi. Platea dictumst quisque sagittis purus sit amet volutpat consequat mauris. Nunc sed augue lacus viverra vitae congue eu consequat ac. Felis donec et odio pellentesque.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
